6c7a09c589b4a815600c440179ea2f8e78189de9
Martin <Martin@DESKTOP-C40APMC>
2024-09-24 16:38:03 -0400

    se agrego la funcion para enviar mensjaes, se modifico el codigo para que se puedan agregar datos a los hashmaps

--- src/main/java/com/mycompany/conexionudp/ConexionUDP.java
+++ src/main/java/com/mycompany/conexionudp/ConexionUDP.java
@@ -9,49 +9,45 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ConexionUDP {
 
+    protected static ConcurrentHashMap<String, ClientHandler> direcciones_ip = new ConcurrentHashMap<>();
+    protected static ConcurrentHashMap<String, ClientHandler> usuarios = new ConcurrentHashMap<>();
+
     public static void main(String[] args) throws IOException {
-        Hashtable<String, Integer> direcciones_ip = new Hashtable<>();
-        Hashtable<String, Integer> usuarios = new Hashtable<>();
+
         //numero de pueerto donde va a escuchar por mensajes.
         final int PUERTO = 5000;
         // buffer para guardar mensajes
         byte[] buffer = new byte[1024];
-        byte[] buffer2 = new byte[1024];
+
 
         try {
             System.out.println("Iniciado el servidor UDP");
-            DatagramSocket socketUDP = new DatagramSocket(PUERTO);
+            DatagramSocket socket = new DatagramSocket(PUERTO);
             while (true) {
                 buffer = new byte[1024];
-                buffer2 = new byte[1024];
                 // crear una peticion, es lo que me va a llegar.
                 DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
 
                 System.out.println("Recibo informacion del cliente...");
 
-                socketUDP.receive(peticion);
-
-                String mensaje = new String(peticion.getData());
-
-                System.out.println("El mensjae es: " + mensaje);
-
+                socket.receive(peticion);
                 int puertoCliente = peticion.getPort();
                 InetAddress direccion = peticion.getAddress();
+                String client_key = direccion.toString() + ":" + puertoCliente;
 
-                String respuestaServer = "¡HOLA respuesta del servidor!";
-
-                buffer2 = respuestaServer.getBytes();
-
-                DatagramPacket respuesta = new DatagramPacket(buffer2, buffer2.length, direccion, puertoCliente);
-
-                System.out.println("envio informacion del cliente...");
-
-                socketUDP.send(respuesta);
+                // Si todavia no se encuentra la direccion ip
+                if (!direcciones_ip.containsKey(client_key)) {
+                    ClientHandler handler = new ClientHandler(socket, direccion, puertoCliente);
+                    direcciones_ip.put(client_key, handler);
+                    new Thread(handler).start();
+                }
+                direcciones_ip.get(client_key).messageHandler(peticion);
             }
 
         } catch (SocketException ex) {
@@ -62,21 +58,70 @@ public class ConexionUDP {
     static class ClientHandler implements Runnable {
 
         private final DatagramSocket socket;
-        private final InetAddress client_address;
-        private final int client_port;
+        private final InetAddress direccion_cliente;
+        private final int puerto_cliente;
         private String user;
         private volatile boolean running = true;
 
-        public ClientHandler (DatagramSocket socket, InetAddress direccion_cliente, int puerto_cliente){
-                this.socket = socket;
-                this.client_address = client_address;
-                this.client_port = client_port;
-                this.user = "";
-            }
+        public ClientHandler(DatagramSocket socket, InetAddress direccion_cliente, int puerto_cliente) {
+            this.socket = socket;
+            this.direccion_cliente = direccion_cliente;
+            this.puerto_cliente = puerto_cliente;
+            this.user = "";
+        }
+
         @Override
         public void run() {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            while (running) {
+
+            }
         }
+
+        public void messageHandler(DatagramPacket mensaje) throws IOException {
+
+            String client_message = new String(mensaje.getData(), 0, mensaje.getLength());
+            System.out.println("Recibido de " + direccion_cliente + ":" + puerto_cliente + " - " + client_message);
+
+            // respuesta del servidor
+            String response;
+            byte[] send;
+            DatagramPacket send_packet;
+             
+            if (client_message.startsWith("usuario:")) {
+                user = client_message.substring(client_message.indexOf(":") + 1);
+                if(!usuarios.containsKey(user)){
+                    usuarios.put(user, this);
+                    response = "usuario agregado: " + user;
+                }  
+                
+                response = "Hola " + user;
+            } else if(usuarios.isEmpty()){
+                response =  "Conectado al servidor. Ingrese 'usuario:(su usuario)'. Una vez ingresado el nombre de usuario, ingrese el nombre de usuario al que le quiera enviar un mensaje, seguido de " + ": y luego el mensaje, de esta forma: 'usuario':'mensaje'";
+            } else{
+                String receptor = client_message.substring(0, client_message.indexOf(":"));
+                if(!usuarios.containsKey(receptor)){
+                    response = "El ususario no se encuentre en la base de datos.";
+               }
+                response = "Mensaje enviado al usuario.";
+                usuarios.get(receptor).sendMessage(user + ":" + client_message.substring(client_message.indexOf(":") + 1));
+        }
+                send = response.getBytes();
+                
+                send_packet = new DatagramPacket(send, send.length, direccion_cliente, puerto_cliente); 
+            
+                socket.send(send_packet);
+        
+    }
+        public void sendMessage(String message) throws IOException {
+            byte[] send;
+            DatagramPacket send_packet;
+            System.out.println("Enviando mensaje a " + message);
+            send = message.getBytes();
+            send_packet = new DatagramPacket(send, send.length, direccion_cliente, puerto_cliente);
+
+            socket.send(send_packet);
 
         }
     }
+
+}
